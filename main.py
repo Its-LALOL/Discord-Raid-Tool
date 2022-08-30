ddcda6fb1647d057f47d4726bc0d2b5c8732e97f
LALOL <89741263+Its-LALOL@users.noreply.github.com>
2022-08-30 21:37:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def load(name):
 	Popen(f'python Functions/{name}.py || python3 Functions/{name}.py')
 def flooder():
 	load('flooder')
-def dmflooder():
-	pass
+def inviter():
+	load('inviter')
 def tokens():
 	system('start Functions/tokens.txt')
 window=Tk()
@@ -16,6 +16,7 @@ window.title('Raid Tool by LALOL | Menu')
 window.geometry('340x666')
 window.config(bg='black')
 Button(text='Flooder', fg='red', bg='grey', font=('Fixedsys', 47), command=flooder).place(x=0, y=0)
+Button(text='Inviter', fg='red', bg='grey', font=('Fixedsys', 47), command=inviter).place(x=0, y=139)
 #Button(text='DM Flooder', fg='red', bg='grey', font=('Fixedsys', 37), command=dmflooder).place(x=-18, y=139)
 Button(text='Tokens', fg='yellow', bg='grey', font=('Fixedsys', 45), command=tokens).place(x=-18, y=555)
 window.mainloop()
